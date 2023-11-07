c98af1873d0ec1018cee829794bf3d07e2906867
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2023-11-07 23:06:04 +0700

    feat: MenuItem dan Snackbar

--- lib/menu.dart
+++ lib/menu.dart
@@ -23,6 +23,87 @@ class HomePage extends StatelessWidget {
           )),
         ),
       ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 10,
+                  bottom: 10,
+                ),
+                child: Text(
+                  "Welcome to Book Collection",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MenuItem {
+  final String name;
+  final IconData icon;
+  final Color? color;
+
+  MenuItem(this.name, this.icon, {this.color});
+}
+
+final List<MenuItem> items = [
+  MenuItem("Lihat Item", Icons.checklist),
+  MenuItem("Tambah Item", Icons.add_shopping_cart),
+  MenuItem("Logout", Icons.logout),
+];
+
+class MenuItemCard extends StatelessWidget {
+  final MenuItem item;
+
+  const MenuItemCard(this.item, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: item.color ?? Colors.blueGrey[700],
+      child: InkWell(
+        onTap: () {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentMaterialBanner()
+            ..showSnackBar(SnackBar(
+                content: Text("Kamu telah menekan tombol ${item.name}!")));
+        },
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 3),
+                  child: Text(
+                    item.name,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
